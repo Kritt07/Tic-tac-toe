9e0fb721fe6c2ae300375b7c1b5bd677d7a406d0
Kritt07 <dum98415@gmail.com>
2026-01-29 16:51:50 +0500

    написал функцию main

--- Tic-tac-toe/Tic-tac-toe.cpp
+++ Tic-tac-toe/Tic-tac-toe.cpp
@@ -9,6 +9,7 @@ const char X = 'X';
 const char O = 'O';
 const char TIE = 'T';
 const char NO_ONE = 'N';
+const char EMPTY = ' ';
 
 // показ инструкций
 void instructions();
@@ -21,14 +22,40 @@ char humanPiece();
 // дает компьютеру оставшуюся фигуру
 char opponent(char piece);
 // отображает поле
-void displayBoard(const vector<string>& board);
+void displayBoard(const vector<char>& board);
 // определяет победителя, возвращает одну из констант
-char winner(const vector<string>& board);
+char winner(const vector<char>& board);
 // определяет, правильно ли сделан ход
-char islegal(const vector<string>& board, int move);
+char islegal(const vector<char>& board, int move);
 // ход пользователя
-int humanMove(const vector<string>& board, char human);
+int humanMove(const vector<char>& board, char human);
 // ход компьтера
-int computerMove(const vector<string>& board, char computer);
+int computerMove(const vector<char>& board, char computer);
 // оглашает результаты
 void announceWinner(char winner, char computer, char human);
+
+int main() {
+	int move;
+	const int NUM_SQUARES = 9;
+	vector<char> board(NUM_SQUARES, EMPTY);
+	instructions();
+	char human = humanPiece();
+	char computer = opponent(human);
+	char turn = X;
+	displayBoard(board);
+	while (winner(board) == NO_ONE) {
+		if (turn == human) {
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else {
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
+	announceWinner(winner(board), computer, human);
+	return 0;
+}
+
