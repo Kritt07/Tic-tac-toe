e12727b65df690517ae083f95708a0c915caf013
Kritt07 <dum98415@gmail.com>
2026-01-30 15:23:53 +0500

    функция humanMove

--- Tic-tac-toe/Tic-tac-toe.cpp
+++ Tic-tac-toe/Tic-tac-toe.cpp
@@ -142,3 +142,13 @@ char winner(const vector<char>& board) {
 inline bool isLegal(int move, const vector<char>& board) {
 	return (board[move] == EMPTY);
 }
+
+int humanMove(const vector<char>& board, char human) {
+	int move = askNumber("Where will you move?", board.size() - 1);
+	while (!isLegal(move, board)) {
+		cout << "\nThat square is already occuped, foolish human.\n";
+		move = askNumber("Where will you move?", board.size() - 1);
+	}
+	cout << "Fine...\n";
+	return move;
+}
